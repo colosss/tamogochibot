72fc3008b38c43d2f3d86d238154acf92847277a
colosss <74936905+colosss@users.noreply.github.com>
2023-10-26 23:05:07 +0300

    a

--- maincode.py
+++ maincode.py
@@ -2,6 +2,7 @@ import telebot
 import helpers
 from telebot import types
 
+
 bot = telebot.TeleBot(helpers.config.token) 
 
 welcome = helpers.start_helpers
@@ -12,35 +13,27 @@ info = helpers.main_info
 
 # Переменные для создания условий->
 
-# def local_namespace():
-# 	global bn
-# 	bn=False
-# 	return bn
-# local_namespace()
-bn=False
-while True:
-	@bot.message_handler(commands=['start'])
-	def send_welcome(message):
-		if bn == False:
-			markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2,selective=True)
-			info.keyboard(markup, 'Начнем!', 'Помощь', 'Какой-то текст')
-			bot.send_photo(message.chat.id, photo=open(welcome.start_pic, 'rb'), caption=welcome.welcome_text, reply_markup=markup)
+bn=False 
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+	if bn !=True:
+		markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2,selective=True)
+		info.keyboard(markup, 'Начнем!', 'Помощь', 'Какой-то текст')
+		bot.send_photo(message.chat.id, photo=open(welcome.start_pic, 'rb'), caption=welcome.welcome_text, reply_markup=markup)
 	# bot.register_next_step_handler
-		elif bn==True:
-			markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2,selective=True)
-			info.keyboard(markup, 'Продолжим!', 'Помощь', 'Какой-то текст')
-			bot.send_photo(message.chat.id, photo=open(welcome.start_pic, 'rb'), caption=f'{welcome.welcome_text}\n{welcome.prodolj_text}', reply_markup=markup)
+	elif bn==True:
+		markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2,selective=True)
+		info.keyboard(markup, 'Продолжим!', 'Помощь', 'Какой-то текст')
+		bot.send_photo(message.chat.id, photo=open(welcome.start_pic, 'rb'), caption=f'{welcome.welcome_text}\n{welcome.prodolj_text}', reply_markup=markup)
 		
-	@bot.message_handler()
-	def send_help(message):
-		if message.text=='/help' or message.text=='Помощь':
-			bot.send_message(message.chat.id, welcome.help_text)
-		if message.text=='Начнем!':
-			bn=True
-			markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2,selective=True)
-			info.keyboard(markup, 'Назад')
-			bot.send_photo(message.chat.id, photo=open(na.nachalo_pic, 'rb'), caption=na.nachalo_text, reply_markup=markup)
-# @bot.message_handler(func=lambda x: x.text=='1')
-# def asdasd(message):
-# 	bot.send_message(message.chat.id, 'dasdasdasd' )
+@bot.message_handler(func=lambda m:True)
+def send_help(message):
+	if message.text=='/help' or message.text=='Помощь':
+		bot.send_message(message.chat.id, welcome.help_text)
+	if message.text=='Начнем!':
+		markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2,selective=True)
+		bn=True
+		info.keyboard(markup, 'Назад')
+		bot.send_photo(message.chat.id, photo=open(na.nachalo_pic, 'rb'), caption=na.nachalo_text, reply_markup=markup)
 bot.infinity_polling()
