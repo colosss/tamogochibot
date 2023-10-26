7a672367b6ce8e4d8f716f9208a7bf98f3133177
colosss <74936905+colosss@users.noreply.github.com>
2023-10-26 20:57:46 +0300

    fg

--- maincode.py
+++ maincode.py
@@ -2,7 +2,6 @@ import telebot
 import helpers
 from telebot import types
 
-markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2, one_time_keyboard=True)
 
 bot = telebot.TeleBot(helpers.config.token) 
 
@@ -14,22 +13,27 @@ info = helpers.main_info
 
 # Переменные для создания условий->
 
-bn=False 
+bn=False
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
 	if bn !=True:
+		markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2,selective=True)
 		info.keyboard(markup, 'Начнем!', 'Помощь', 'Какой-то текст')
 		bot.send_photo(message.chat.id, photo=open(welcome.start_pic, 'rb'), caption=welcome.welcome_text, reply_markup=markup)
+		# return bn = True
 	# bot.register_next_step_handler
-	else:
+	elif bn==True:
+		markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2,selective=True)
 		info.keyboard(markup, 'Продолжим!', 'Помощь', 'Какой-то текст')
 		bot.send_photo(message.chat.id, photo=open(welcome.start_pic, 'rb'), caption=f'{welcome.welcome_text}\n{welcome.prodolj_text}', reply_markup=markup)
+		
 @bot.message_handler(func=lambda m:True)
 def send_help(message):
 	if message.text=='/help' or message.text=='Помощь':
 		bot.send_message(message.chat.id, welcome.help_text)
 	if message.text=='Начнем!':
+		markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2,selective=True)
 		bn=True
 		info.keyboard(markup, 'Назад')
 		bot.send_photo(message.chat.id, photo=open(na.nachalo_pic, 'rb'), caption=na.nachalo_text, reply_markup=markup)
