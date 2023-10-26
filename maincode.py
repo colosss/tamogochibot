3a9812702c3d4d62e47cf4d24710570e94929b93
colosss <74936905+colosss@users.noreply.github.com>
2023-10-26 23:26:08 +0300

    gf

--- maincode.py
+++ maincode.py
@@ -13,27 +13,28 @@ info = helpers.main_info
 
 # Переменные для создания условий->
 
-bn=False 
+bn=helpers.start_helpers.b1
+
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-	if bn !=True:
+	if helpers.start_helpers.bn !=True:
 		markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2,selective=True)
 		info.keyboard(markup, 'Начнем!', 'Помощь', 'Какой-то текст')
 		bot.send_photo(message.chat.id, photo=open(welcome.start_pic, 'rb'), caption=welcome.welcome_text, reply_markup=markup)
 	# bot.register_next_step_handler
-	elif bn==True:
+	elif helpers.start_helpers.bn==True:
 		markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2,selective=True)
 		info.keyboard(markup, 'Продолжим!', 'Помощь', 'Какой-то текст')
 		bot.send_photo(message.chat.id, photo=open(welcome.start_pic, 'rb'), caption=f'{welcome.welcome_text}\n{welcome.prodolj_text}', reply_markup=markup)
-		
 @bot.message_handler(func=lambda m:True)
 def send_help(message):
 	if message.text=='/help' or message.text=='Помощь':
 		bot.send_message(message.chat.id, welcome.help_text)
 	if message.text=='Начнем!':
 		markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2,selective=True)
-		bn=True
+		# global a
+		helpers.start_helpers.bn=True
 		info.keyboard(markup, 'Назад')
 		bot.send_photo(message.chat.id, photo=open(na.nachalo_pic, 'rb'), caption=na.nachalo_text, reply_markup=markup)
 bot.infinity_polling()
